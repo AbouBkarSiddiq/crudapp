5a76080700a77188103fda1b23358dc2566df80a
awaisamir123 <awaisamir23@gmail.com>
2021-11-26 17:39:43 +0500

    message

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3001;
-const publicPath = path.join(__dirname, '../public');
-
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '..', 'public');
+console.log("publicPath",publicPath)
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
