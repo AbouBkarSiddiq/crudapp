210558ec832645c0e3b4f605390de0dc65552dbf
awaisamir123 <awaisamir23@gmail.com>
2021-11-26 18:05:50 +0500

    message

--- server/server.js
+++ server/server.js
@@ -1,28 +1,13 @@
-// const path = require('path');
-// const express = require('express');
-// const app = express();
-// const port = process.env.PORT || 3000;
-// const publicPath = path.join(__dirname, '..', 'public');
-// console.log("publicPath",publicPath)
-// app.use(express.static(publicPath));
-
-// app.get('*', (req, res) => {
-//    res.sendFile(path.join(publicPath, 'index.html'));
-// });
-
-// app.listen(port, () => {
-//    console.log('Server is up!');
-// });
-
-const express = require('express');
 const path = require('path');
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '..', 'public');
+console.log("publicPath",publicPath)
+app.use(express.static(publicPath));
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 app.listen(port, () => {
